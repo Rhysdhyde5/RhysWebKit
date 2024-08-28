8fbae742c2f4c21d7a99da3a92a7ed3179a5ffc1
Rhysdhyde5 <2120303@swansea.ac.uk>
2024-08-28 21:09:28 +0100

    added cool buttons to nav bar

--- RhysWebApp/src/components/Navbar.svelte
+++ RhysWebApp/src/components/Navbar.svelte
@@ -14,11 +14,19 @@
   </nav>
   
   <style>
-    .navbar {
-      background-color: #333;
-      padding: 1rem;
+
+    body {
+      margin: 0;
+      padding: 0;
     }
-    
+  
+    .navbar {
+      background-color: #38443877;
+      padding: 1rem;
+      border-radius: 25px;
+      scale: 1.1;
+    }
+  
     .navbar ul {
       list-style-type: none;
       display: flex;
@@ -26,18 +34,57 @@
       margin: 0;
       padding: 0;
     }
-    
+  
     .navbar ul li {
       margin: 0 1rem;
     }
-    
+  
     .navbar ul li a {
+      position: relative;
+      padding: 10px 20px;
       color: #fff;
       text-decoration: none;
       font-weight: bold;
+      background: rgba(255, 255, 255, 0);
+      font-size: 16px;
+      border-top-right-radius: 10px;
+      border-bottom-left-radius: 10px;
+      transition: all 1s;
+      color: rgb(255, 255, 255); /* Set text color to white */
     }
-    
+  
+    .navbar ul li a:after, 
+    .navbar ul li a:before {
+      content: " ";
+      width: 10px;
+      height: 10px;
+      position: absolute;
+      border: 0px solid #ffffff00;
+      transition: all 1s;
+    }
+  
+    .navbar ul li a:after {
+      top: -1px;
+      left: -1px;
+      border-top: 5px solid rgb(255, 255, 255);
+      border-left: 5px solid rgb(255, 255, 255);
+    }
+  
+    .navbar ul li a:before {
+      bottom: -1px;
+      right: -1px;
+      border-bottom: 5px solid rgb(255, 255, 255);
+      border-right: 5px solid rgb(255, 255, 255);
+    }
+  
     .navbar ul li a:hover {
-      text-decoration: underline;
+      border-top-right-radius: 0px;
+      border-bottom-left-radius: 0px;
+    }
+  
+    .navbar ul li a:hover:before, 
+    .navbar ul li a:hover:after {
+      width: 100%;
+      height: 100%;
     }
   </style>
